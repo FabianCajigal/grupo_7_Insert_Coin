e2f8928e8722a5628a95ef68c3548eb306192cce
lpatyn <lautaropatyn@outlook.com>
2022-12-12 01:14:03 -0300

    Creado componente header + estilos

--- src/app.js
+++ src/app.js
@@ -7,3 +7,5 @@ app.use( express.static('public') );
 app.listen( 3030, () => console.log('Server running on http://localhost:3030') );
 
 app.get( '/', (req, res) => res.sendFile( path.resolve(__dirname, './views/home.html') ));
+
+app.get( '/register', (req, res) => res.sendFile( path.resolve(__dirname, './views/register.html') ));
