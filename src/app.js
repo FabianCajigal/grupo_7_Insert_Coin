02ad45cc55cb60419c12fcd0fb2607d4cd9fbef3
lpatyn <lautaropatyn@outlook.com>
2022-12-15 23:55:37 -0300

    Creacion de rutas y links provisorios para testing en el home

--- src/app.js
+++ src/app.js
@@ -8,4 +8,10 @@ app.listen( 3030, () => console.log('Server running on http://localhost:3030') )
 
 app.get( '/', (req, res) => res.sendFile( path.resolve(__dirname, './views/home.html') ));
 
+app.get( '/login', (req, res) => res.sendFile( path.resolve(__dirname, './views/login.html') ));
+
 app.get( '/register', (req, res) => res.sendFile( path.resolve(__dirname, './views/register.html') ));
+
+app.get( '/productDetail', (req, res) => res.sendFile( path.resolve(__dirname, './views/productDetail.html') ));
+
+app.get( '/productCart', (req, res) => res.sendFile( path.resolve(__dirname, './views/productCart.html') ));
