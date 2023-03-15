5e22cf964794b2a26dbb82248c4f49731d93c1fc
lpatyn <lautaropatyn@outlook.com>
2023-03-14 21:13:35 -0300

    Migracion completa del controlador de usuarios a sistema sequelize

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,8 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 const db = require('../database/models');
-const dataPath = path.resolve(__dirname, '../data/usersDatabase.json');
-const users = require('../data/usersDataBase.json');
 const bcrypt = require('bcryptjs');
 
 const userController = {
@@ -15,7 +13,7 @@ const userController = {
             admin: req.body.category,
             password: bcrypt.hashSync(req.body.password, 10),
             image: req.file ? req.file.filename : 'default-image.png'
-        }).then ( (user) => {
+        }).then( user => {
             req.session.user = { 
                 id: user.id,
                 username: user.username,
@@ -33,13 +31,13 @@ const userController = {
             where: {
                 username: req.body.username
             }
-        }).then ( (user) => {
+        }).then( user => {
             if (user && bcrypt.compareSync(req.body.password, user.password)) {
             
                 if (req.body.rememberMe != undefined) {
                     res.cookie('id', user.id, { maxAge: 2592000000, httpOnly: true });
                 }
-    
+
                 req.session.user = { 
                     id: user.id,
                     username: user.username,
@@ -56,43 +54,56 @@ const userController = {
     profile: (req, res) => res.render('profile'),
     edit: (req, res) => {
         db.User.findByPk (req.session.user.id)
-            .then ( (user) => {
+            .then( user => {
                 res.render('userEdit', { user: user });
             });
     },
     update: (req,res) => {
-        const userToEdit = users.find(user => user.id == req.session.user.id);
-        const index = users.findIndex(user => user.id == req.session.user.id);
-        const editedUser = {
-            id: userToEdit.id,
-            email: req.body.email,
-            username: req.body.username, 
-            category: req.body.category,
-            password: userToEdit.password,
-            image: req.file ? req.file.filename : userToEdit.image 
-        };
-        if (req.file && userToEdit.image != 'default-image.png') {
-            fs.unlinkSync(path.resolve(__dirname, `../../public/img/users/${userToEdit.image}`));
-        }
-        users[index] = editedUser;
-        fs.writeFileSync(dataPath, JSON.stringify(users, null, ' '));
-        req.session.user.username = editedUser.username;
-        req.session.user.category = editedUser.category;
-        res.redirect('/account');
+        db.User.update(
+            {
+                email: req.body.email,
+                username: req.body.username, 
+                admin: req.body.category,
+                image: req.file ? req.file.filename : req.session.user.image 
+            },
+            {
+                where: { id: req.session.user.id }
+            })
+                .then( () => {
+                    db.User.findByPk (req.session.user.id)
+                        .then( user => {
+                            if (req.file && req.session.user.image != 'default-image.png') {
+                                fs.unlinkSync(path.resolve(__dirname, `../../public/img/users/${req.session.user.image}`));
+                            };
+                            req.session.user.username = user.username;
+                            req.session.user.admin = user.admin;
+                            req.session.user.image = user.image;
+                            res.redirect('/account');
+                        });
+                });
     },
     passwordChange: (req, res) => {
         res.render('passwordChange');
     },
     passwordUpdate: (req, res) => {
-        const index = users.findIndex(user => user.id == req.session.user.id);
-        if (bcrypt.compareSync(req.body.oldPassword, users[index].password) && req.body.password == req.body.passwordConfirm) {
-            users[index].password = bcrypt.hashSync(req.body.password, 10);
-            fs.writeFileSync(dataPath, JSON.stringify(users, null, ' '));
-            res.redirect('/account');
-        }
-        else {
-            res.render('passwordChange');
-        }
+        db.User.findByPk (req.session.user.id)
+            .then( user => {
+                if (bcrypt.compareSync(req.body.oldPassword, user.password) && req.body.password == req.body.passwordConfirm) {
+                    db.User.update(
+                        {
+                            password: bcrypt.hashSync(req.body.password, 10) 
+                        },
+                        {
+                            where: { id: req.session.user.id }
+                        })
+                            .then( () => {
+                                res.redirect('/account');
+                            });
+                }
+                else {
+                    res.render('passwordChange');
+                }
+            });   
     },
     logout: (req, res) => {
         res.clearCookie('id');
